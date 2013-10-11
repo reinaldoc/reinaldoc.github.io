b2ce254bee301488897ef44792579f5c8e5b0f15
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2013-10-11 20:05:02 -0300

    update maven script

--- tools/repo.sh
+++ tools/repo.sh
@@ -17,7 +17,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 
 # Maven Repository Path
-REPOPATH=/opt/git/reinaldoc.github.com/maven
+REPOPATH=/opt/github/reinaldoc.github.io/maven
 
 # pom.xml Relative path inside jar file
 POMPATH=pom.xml
